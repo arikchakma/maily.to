29d49045693a8e88de160a9e556d5edf5ee068d7
Arik Chakma <arikchangma@gmail.com>
2023-06-27 01:34:44 +0600

    chore: bubble menu

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -4,7 +4,7 @@ import { Color } from '@tiptap/extension-color'
 import ListItem from '@tiptap/extension-list-item'
 import TextStyle from '@tiptap/extension-text-style'
 import Heading from '@tiptap/extension-heading'
-import { EditorContent, useEditor } from '@tiptap/react'
+import { BubbleMenu, EditorContent, useEditor } from '@tiptap/react'
 import { Editor as EditorType } from '@tiptap/core'
 import TextAlign from '@tiptap/extension-text-align'
 import React from 'react'
@@ -12,6 +12,11 @@ import Paragraph from '@tiptap/extension-paragraph'
 import Document from '@tiptap/extension-document'
 import Text from '@tiptap/extension-text'
 import Bold from '@tiptap/extension-bold'
+import Italic from '@tiptap/extension-italic'
+import Strike from '@tiptap/extension-strike'
+import { Button } from '../ui/button'
+import { AlignCenter, AlignLeft, AlignRight, BoldIcon, ItalicIcon, StrikethroughIcon } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 const MenuBar = ({ editor }: {
   editor: EditorType | null
@@ -66,44 +71,6 @@ const MenuBar = ({ editor }: {
         h3
       </button>
       <button
-        onClick={() => {
-          // If the selected node has style textAlign `center`, make it `left`
-          if (editor.isActive({ textAlign: 'center' })) {
-            editor.chain().focus().unsetTextAlign().run()
-          } else {
-            editor.chain().focus().setTextAlign('center').run()
-          }
-        }}
-        className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}
-      >Center</button>
-      <button
-        onClick={() => {
-          // If the selected node has style textAlign `center`, make it `left`
-          if (editor.isActive({ textAlign: 'left' })) {
-            editor.chain().focus().unsetTextAlign().run()
-          } else {
-            editor.chain().focus().setTextAlign('left').run()
-          }
-        }}
-        className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}
-      >
-        Left
-      </button>
-      <button
-        onClick={() => {
-          // If the selected node has style textAlign `center`, make it `left`
-          if (editor.isActive({ textAlign: 'right' })) {
-            editor.chain().focus().unsetTextAlign().run()
-          } else {
-            editor.chain().focus().setTextAlign('right').run()
-          }
-        }}
-        className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}
-      >
-        Right
-      </button>
-
-      <button
         onClick={() => editor.chain().focus().setColor('#958DF1').run()}
         className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
       >
@@ -126,9 +93,11 @@ export function Editor() {
       Paragraph,
       Text,
       Bold,
+      Italic,
+      Strike,
       Color.configure({ types: [TextStyle.name, ListItem.name] }),
       TextStyle.configure(),
-      TextAlign.configure({ types: [Paragraph.name, Heading.name], defaultAlignment: "left", }),
+      TextAlign.configure({ types: [Paragraph.name, Heading.name] }),
       Heading.extend({
         levels: [1, 2, 3],
       }),
@@ -145,7 +114,81 @@ export function Editor() {
   return (
     <div>
       <MenuBar editor={editor} />
+      {editor && <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }} className='flex gap-1 rounded-md border border-slate-200 bg-white p-1 shadow-md'>
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          data-state={editor.isActive('bold') ? 'on' : 'off'}
+          className={cn('px-2.5')}
+        >
+          <BoldIcon className="h-3.5 w-3.5" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          data-state={editor.isActive('italic')}
+          className={cn('px-2.5')}
+        >
+          <ItalicIcon className="h-3.5 w-3.5" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          data-state={editor.isActive('strike')}
+          className={cn('px-2.5')}
+        >
+          <StrikethroughIcon className="h-3.5 w-3.5" />
+        </Button >
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => {
+            // If the selected node has style textAlign `center`, make it `left`
+            if (editor.isActive({ textAlign: 'left' })) {
+              editor.chain().focus().unsetTextAlign().run()
+            } else {
+              editor.chain().focus().setTextAlign('left').run()
+            }
+          }}
+          className={cn('px-2.5', editor.isActive('strike') ? 'is-active' : '')}
+        >
+          <AlignLeft className="h-3.5 w-3.5" />
+        </Button >
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => {
+            // If the selected node has style textAlign `center`, make it `left`
+            if (editor.isActive({ textAlign: 'center' })) {
+              editor.chain().focus().unsetTextAlign().run()
+            } else {
+              editor.chain().focus().setTextAlign('center').run()
+            }
+          }}
+          className={cn('px-2.5', editor.isActive('strike') ? 'is-active' : '')}
+        >
+          <AlignCenter className="h-3.5 w-3.5" />
+        </Button >
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => {
+            // If the selected node has style textAlign `center`, make it `left`
+            if (editor.isActive({ textAlign: 'right' })) {
+              editor.chain().focus().unsetTextAlign().run()
+            } else {
+              editor.chain().focus().setTextAlign('right').run()
+            }
+          }}
+          className={cn('px-2.5', editor.isActive('strike') ? 'is-active' : '')}
+        >
+          <AlignRight className="h-3.5 w-3.5" />
+        </Button >
+      </BubbleMenu >}
       <EditorContent editor={editor} />
-    </div>
+    </div >
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 }

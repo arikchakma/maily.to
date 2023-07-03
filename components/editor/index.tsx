90cb3ccb2bf5257a8b79fbcff62b779a66ba49fe
Arik Chakma <arikchangma@gmail.com>
2023-07-03 21:16:36 +0600

    chore: max width 600px

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -17,17 +17,165 @@ export function Editor() {
       }
     },
     extensions: TiptapExtensions,
-    content: `
-      <h1>
-        Hi there,
-      </h1>
-    `,
+    content: {
+      "type": "doc",
+      "content": [
+        {
+          "type": "paragraph",
+          "attrs": {
+            "textAlign": "left"
+          }
+        },
+        {
+          "type": "logo",
+          "attrs": {
+            "src": "https://binsta.dev/api/v1/files/4eR1893USp/transform?format=webp&size=lg&quality=md",
+            "alt": null,
+            "title": null,
+            "mailbox-component": "logo",
+            "size": "sm",
+            "alignment": "left"
+          }
+        },
+        {
+          "type": "paragraph",
+          "attrs": {
+            "textAlign": "left"
+          },
+          "content": [
+            {
+              "type": "text",
+              "text": "Hey "
+            },
+            {
+              "type": "variable",
+              "attrs": {
+                "id": "username",
+                "label": null
+              }
+            },
+            {
+              "type": "text",
+              "text": ","
+            }
+          ]
+        },
+        {
+          "type": "horizontalRule"
+        },
+        {
+          "type": "paragraph",
+          "attrs": {
+            "textAlign": "left"
+          },
+          "content": [
+            {
+              "type": "text",
+              "text": "Thank you so much for joining the waitlist. We are excited to welcome you to the [product name] community."
+            }
+          ]
+        },
+        {
+          "type": "paragraph",
+          "attrs": {
+            "textAlign": "left"
+          },
+          "content": [
+            {
+              "type": "text",
+              "text": "Stay tuned. And we're just an email away if you have any questions. We'd be more than happy to answer your questions."
+            }
+          ]
+        },
+        {
+          "type": "spacer",
+          "attrs": {
+            "mailbox-component": "spacer",
+            "height": "xl"
+          }
+        },
+        {
+          "type": "paragraph",
+          "attrs": {
+            "textAlign": "left"
+          },
+          "content": [
+            {
+              "type": "text",
+              "text": "Cheers,"
+            },
+            {
+              "type": "hardBreak"
+            },
+            {
+              "type": "text",
+              "text": "James, "
+            },
+            {
+              "type": "text",
+              "marks": [
+                {
+                  "type": "italic"
+                }
+              ],
+              "text": "creator of [product name]"
+            }
+          ]
+        },
+        {
+          "type": "horizontalRule"
+        },
+        {
+          "type": "footer",
+          "attrs": {
+            "mailbox-component": "footer"
+          },
+          "content": [
+            {
+              "type": "text",
+              "text": "You are receiving this email because you joined the waitlist for [product name]."
+            }
+          ]
+        },
+        {
+          "type": "footer",
+          "attrs": {
+            "mailbox-component": "footer"
+          },
+          "content": [
+            {
+              "type": "text",
+              "text": "© 2023 [Product name]"
+            },
+            {
+              "type": "hardBreak"
+            },
+            {
+              "type": "text",
+              "text": "[address]"
+            }
+          ]
+        },
+        {
+          "type": "footer",
+          "attrs": {
+            "mailbox-component": "footer"
+          },
+          "content": [
+            {
+              "type": "text",
+              "text": "Unsubscribe from emails"
+            }
+          ]
+        }
+      ]
+    },
   })
 
-  console.log(editor?.getJSON())
+  console.log(editor?.getHTML())
 
   return (
-    <div className="rounded border">
+    <div className="rounded border max-w-[600px]">
       <div className="p-2">
         <EditorMenuBar editor={editor} />
       </div>

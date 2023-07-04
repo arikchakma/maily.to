6f57ef4aad8903c14b6562062ef4d3fa8c9dcd74
Arik Chakma <arikchangma@gmail.com>
2023-07-04 18:06:59 +0600

    chore: pattern into tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Editor } from "@/components/editor";
+import { Editor } from '@/components/editor';
 
 export default function Home() {
-  return (
-    <div className="p-10 pattern-background min-h-screen">
-      <Editor />
-    </div>
-  )
+	return (
+		<div className="p-10 bg-white opacity-100 bg-[radial-gradient(#000000_0.65px,transparent_0.65px),radial-gradient(#000000_0.65px,#ffffff_0.65px)] [background-size:26px_26px] [background-position:0_0,13px_13px] min-h-screen">
+			<Editor />
+		</div>
+	);
 }

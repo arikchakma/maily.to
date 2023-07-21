b2e6316aff6bab31f9d04207d2eca10971e5c2ee
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-07-21 01:27:38 +0100

    Homepage design

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function LandingPage() {
                 Open-source editor for crafting emails.
               </p>
               <div className="mt-4 flex flex-col items-stretch gap-2 sm:flex-row md:mt-8 lg:mt-10">
-                <button className="flex items-center justify-center gap-3 rounded-xl bg-black px-7 py-3 text-2xl font-medium text-white hover:bg-red-500 focus:outline-0">
+                <button className="flex items-center justify-center gap-3 rounded-xl bg-black px-7 py-3 text-2xl font-medium text-white hover:bg-red-500 focus:outline-0 transition-all">
                   <PencilIcon />
                   Open Editor
                 </button>
@@ -74,7 +74,8 @@ export default function LandingPage() {
         <div className="mx-auto max-w-[1050px] px-7">
           <h2 className="mb-6 text-6xl font-bold">
             Designing Emails is{' '}
-            <span className="line-through">&nbsp;Hard&nbsp;</span> Easy
+            <span className="text-red-500 line-through">&nbsp;Hard&nbsp;</span>{' '}
+            Easy
           </h2>
           <p className="mb-14 text-3xl leading-relaxed">
             Maily is a free and{' '}
@@ -124,19 +125,83 @@ export default function LandingPage() {
         </div>
       </div>
 
+      <div className="border-b-8 border-b-black py-24">
+        <div className="mx-auto max-w-[1050px] px-7">
+          <h2 className="mb-6 text-6xl font-bold">
+            <span className="text-red-400 line-through">
+              &nbsp;So Many&nbsp;
+            </span>{' '}
+            Not Many Stars
+          </h2>
+          <p className="mb-4 text-3xl leading-relaxed">
+            I just launched the project —— thus, not many stars.
+          </p>
+          <p className="mb-7 text-3xl leading-relaxed">
+            Please do{' '}
+            <a
+              href="#"
+              className="underline underline-offset-8 hover:text-red-500"
+            >
+              leave a star on GitHub
+            </a>{' '}
+            if you like the project. Also, I would love to hear from you if you
+            have any feedback or suggestions.
+          </p>
+
+          <div className='flex gap-3'>
+            <a
+              href="https://github.com/arikchakma/maily.to"
+              data-github-link=""
+              target="_blank"
+              className="inline-flex items-center justify-center rounded-lg border-black bg-red-300 px-5 py-2 text-lg font-medium hover:bg-red-400 md:text-xl lg:rounded-xl lg:py-4 lg:text-2xl transition-colors"
+            >
+              <span className="mr-3 inline-flex items-center">
+                <img
+                  src="/star.svg"
+                  alt="Hero Image"
+                  className="mr-1 h-5 md:mr-2 md:h-7"
+                />{' '}
+                <span className="line-through">&nbsp;100k&nbsp;</span>
+              </span>
+              GitHub Stars
+            </a>
+
+            <a href="/docs/installation"
+               className="bg-white gap-2 justify-center text-black flex items-center font-medium text-lg md:text-xl lg:text-2xl border-4 border-black rounded-lg lg:rounded-xl py-2 lg:py-3 px-5 hover:bg-black hover:text-white transition-colors">
+              <PencilIcon />
+              Open Editor
+            </a>
+          </div>
+        </div>
+      </div>
+
       <div className="bg-black py-8 text-white">
         <div className="mx-auto max-w-[1050px] px-7 sm:px-10">
           <p className="text-center text-lg text-white">
             MIT Licensed © 2023
             <span className="mx-3">·</span>
-            <a href="/docs/installation" className="hover:text-red-300 inline-flex items-center">
+            <a
+              href="/docs/installation"
+              className="inline-flex items-center hover:text-red-300"
+            >
               GitHub
-              <img src="/arrow.svg" className="h-3 inline-block ml-2" alt="GitHub" />
+              <img
+                src="/arrow.svg"
+                className="ml-2 inline-block h-3"
+                alt="GitHub"
+              />
             </a>
             <span className="mx-3">·</span>
-            <a href="/docs/installation" className="hover:text-red-300 inline-flex items-center">
+            <a
+              href="/docs/installation"
+              className="inline-flex items-center hover:text-red-300"
+            >
               Twitter
-              <img src="/arrow.svg" className="h-3 inline-block ml-2" alt="Twitter" />
+              <img
+                src="/arrow.svg"
+                className="ml-2 inline-block h-3"
+                alt="Twitter"
+              />
             </a>
           </p>
         </div>

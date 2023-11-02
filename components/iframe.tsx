42ecfb46c1da79a3e1ae3ad739d3e1c0681fd296
Munkhbaatar Ganbaatar <ganbaa.moogii@gmail.com>
2023-11-02 23:53:32 +0800

    fix: blank iframe issue (#18)
    * fix: blank iframe issue
    
    * fix: clear timeout
    
    * chore: remove timeout
    
    * fix: iframe issue on firefox
    
    * Fix `iFrame` browser issue
    
    ---------
    
    Co-authored-by: Arik Chakma <arikchangma@gmail.com>

--- components/iframe.tsx
+++ components/iframe.tsx
@@ -1,6 +1,27 @@
 'use client';
 
-import { useRef } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
+
+import { isSafari } from '@/utils/detect-browser';
+
+function renderHTMLToIFrame(ref: RefObject<HTMLIFrameElement>, html: string) {
+  if (!ref.current) {
+    return;
+  }
+  const iframeDocument = ref.current.contentDocument;
+
+  if (!iframeDocument) {
+    return;
+  }
+  iframeDocument.body.style.padding = '20px 0';
+  const fontLink = iframeDocument.createElement('link');
+  fontLink.rel = 'stylesheet';
+  fontLink.href =
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap';
+  iframeDocument.head.appendChild(fontLink);
+
+  iframeDocument.body.innerHTML = html;
+}
 
 export const IFrame = ({
   innerHTML,
@@ -10,24 +31,24 @@ export const IFrame = ({
 } & React.HTMLProps<HTMLIFrameElement>) => {
   const contentRef = useRef<HTMLIFrameElement>(null);
 
+  useEffect(() => {
+    if (!isSafari()) {
+      return;
+    }
+
+    renderHTMLToIFrame(contentRef, innerHTML);
+  }, [innerHTML]);
+
   return (
     <iframe
       {...props}
       ref={contentRef}
-      onLoad={(e) => {
-        const iframe = e.target as HTMLIFrameElement;
-        const iframeDocument = iframe.contentDocument;
-        if (!iframeDocument) {
+      onLoad={() => {
+        if (isSafari()) {
           return;
         }
-        iframeDocument.body.style.padding = '20px 0';
-        const fontLink = iframeDocument.createElement('link');
-        fontLink.rel = 'stylesheet';
-        fontLink.href =
-          'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap';
-        iframeDocument.head.appendChild(fontLink);
 
-        iframeDocument.body.innerHTML = innerHTML;
+        renderHTMLToIFrame(contentRef, innerHTML);
       }}
     />
   );

0c0ba7360005efc19c5a4f59c5739608fe0858d9
ganbaa.moogii <ganbaa.moogii@gmail.com>
2023-11-02 21:04:57 +0800

    fix: blank iframe issue

--- components/iframe.tsx
+++ components/iframe.tsx
@@ -2,7 +2,42 @@
 
 import { useRef } from 'react';
 
+export const IFrame = ({'use client';
+
+import { useEffect, useRef } from 'react';
+
 export const IFrame = ({
+  innerHTML,
+  ...props
+}: {
+  innerHTML: string;
+} & React.HTMLProps<HTMLIFrameElement>) => {
+  const contentRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!contentRef.current) {
+        return;
+      }
+      const iframeDocument = contentRef.current.contentDocument;
+
+      if (!iframeDocument) {
+        return;
+      }
+      iframeDocument.body.style.padding = '20px 0';
+      const fontLink = iframeDocument.createElement('link');
+      fontLink.rel = 'stylesheet';
+      fontLink.href =
+        'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap';
+      iframeDocument.head.appendChild(fontLink);
+
+      iframeDocument.body.innerHTML = innerHTML;
+    }, 200);
+  }, [innerHTML]);
+
+  return <iframe {...props} ref={contentRef} />;
+};
+
   innerHTML,
   ...props
 }: {

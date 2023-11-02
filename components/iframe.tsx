6f30e3142aecadff94ca40ac85ef50fb290c2d9b
ganbaa.moogii <ganbaa.moogii@gmail.com>
2023-11-02 21:42:14 +0800

    chore: remove timeout

--- components/iframe.tsx
+++ components/iframe.tsx
@@ -11,28 +11,22 @@ export const IFrame = ({
   const contentRef = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (!contentRef.current) {
-        return;
-      }
-      const iframeDocument = contentRef.current.contentDocument;
+    if (!contentRef.current) {
+      return;
+    }
+    const iframeDocument = contentRef.current.contentDocument;
 
-      if (!iframeDocument) {
-        return;
-      }
-      iframeDocument.body.style.padding = '20px 0';
-      const fontLink = iframeDocument.createElement('link');
-      fontLink.rel = 'stylesheet';
-      fontLink.href =
-        'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap';
-      iframeDocument.head.appendChild(fontLink);
+    if (!iframeDocument) {
+      return;
+    }
+    iframeDocument.body.style.padding = '20px 0';
+    const fontLink = iframeDocument.createElement('link');
+    fontLink.rel = 'stylesheet';
+    fontLink.href =
+      'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap';
+    iframeDocument.head.appendChild(fontLink);
 
-      iframeDocument.body.innerHTML = innerHTML;
-    }, 200);
-
-    return () => {
-      clearTimeout(timeout);
-    };
+    iframeDocument.body.innerHTML = innerHTML;
   }, [innerHTML]);
 
   return <iframe {...props} ref={contentRef} />;

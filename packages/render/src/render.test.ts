e2330232e70c7f4228f4c3b2dd18b5f90f608ca7
Arik Chakma <arikchangma@gmail.com>
2023-12-03 15:30:04 +0600

    Add test for formatter

--- packages/render/src/render.test.ts
+++ packages/render/src/render.test.ts
@@ -78,4 +78,34 @@ describe('render', () => {
     });
     expect(result).toMatchInlineSnapshot(`"{{name,fallback=Buddy}}"`);
   });
+
+  it('should replace variables formatter with custom formatter', () => {
+    const content = {
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'variable',
+              attrs: {
+                id: 'name',
+                fallback: 'Buddy',
+              },
+            },
+          ],
+        },
+      ],
+    };
+    const result = render(content, {
+      options: {
+        plainText: true,
+      },
+      variableFormatter(options) {
+        const { fallback, variable } = options;
+        return `[${variable},fallback=${fallback}]`;
+      },
+    });
+    expect(result).toMatchInlineSnapshot(`"[name,fallback=Buddy]"`);
+  });
 });

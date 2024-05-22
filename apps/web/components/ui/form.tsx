3382420ca9ec9de11c8d0c2297f8b39302587f6f
Arik Chakma <arikchangma@gmail.com>
2024-05-22 15:42:29 +0600

    fix: build error

--- apps/web/components/ui/form.tsx
+++ apps/web/components/ui/form.tsx
@@ -103,9 +103,7 @@ const FormControl = React.forwardRef<
   return (
     <Slot
       aria-describedby={
-        !error
-          ? `${formDescriptionId}`
-          : `${formDescriptionId} ${formMessageId}`
+        !error ? formDescriptionId : `${formDescriptionId} ${formMessageId}`
       }
       aria-invalid={Boolean(error)}
       id={formItemId}

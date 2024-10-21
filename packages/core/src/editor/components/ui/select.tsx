02e377a8011a613b7eb7db15a9dcc002f191c7c1
Arik Chakma <arikchangma@gmail.com>
2024-10-21 16:58:30 +0600

    fix: remove leading

--- packages/core/src/editor/components/ui/select.tsx
+++ packages/core/src/editor/components/ui/select.tsx
@@ -30,7 +30,7 @@ export function Select(props: SelectProps) {
       <select
         id={selectId}
         className={cn(
-          'mly-flex mly-min-h-7 mly-max-w-max mly-appearance-none mly-items-center mly-rounded-md mly-px-1.5 mly-py-0.5 mly-text-sm mly-leading-none mly-text-midnight-gray hover:mly-bg-soft-gray focus:mly-outline-none active:mly-bg-soft-gray',
+          'mly-flex mly-min-h-7 mly-max-w-max mly-appearance-none mly-items-center mly-rounded-md mly-px-1.5 mly-py-0.5 mly-text-sm mly-text-midnight-gray hover:mly-bg-soft-gray focus:mly-outline-none active:mly-bg-soft-gray',
           className
         )}
         value={value}

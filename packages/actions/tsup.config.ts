76689ace84d458872c6ef6f2aa02bdd2c2dc3251
Youssef Elkhayami <youssef@fonk.amsterdam>
2024-11-07 14:35:01 +0100

    better config

--- packages/actions/tsup.config.ts
+++ packages/actions/tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,
+  format: ['iife'],
+  dts: false,
   minify: true,
   clean: true,
 });

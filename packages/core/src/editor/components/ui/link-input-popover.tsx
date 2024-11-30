920a724524104f09209700a339dc39a0103a0660
Arik Chakma <arikchangma@gmail.com>
2024-11-30 11:07:54 +0600

    feat: add `https://` prefix

--- packages/core/src/editor/components/ui/link-input-popover.tsx
+++ packages/core/src/editor/components/ui/link-input-popover.tsx
@@ -20,6 +20,10 @@ export function LinkInputPopover(props: LinkInputPopoverProps) {
     icon: Icon = Link,
   } = props;
 
+  const defaultUrl = defaultValue.startsWith('https://')
+    ? defaultValue.replace('https://', '')
+    : defaultValue;
+
   const [isOpen, setIsOpen] = useState(false);
   const linkInputRef = useRef<HTMLInputElement>(null);
 
@@ -30,12 +34,20 @@ export function LinkInputPopover(props: LinkInputPopoverProps) {
         size="sm"
         type="button"
         className="mly-size-7"
-        data-state={!!defaultValue}
+        data-state={!!defaultUrl}
       >
         <Icon className="mly-h-3 mly-w-3 mly-shrink-0 mly-stroke-[2.5] mly-text-midnight-gray" />
       </BaseButton>
     </PopoverTrigger>
   );
+
+  const normalizeToHttps = (value: string) => {
+    if (value.startsWith('https://')) {
+      return value;
+    }
+
+    return `https://${value}`;
+  };
 
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
@@ -51,7 +63,7 @@ export function LinkInputPopover(props: LinkInputPopoverProps) {
       <PopoverContent
         align="end"
         side="top"
-        className="mly-w-max mly-rounded-lg !mly-p-0.5"
+        className="mly-w-max mly-rounded-none mly-border-none mly-bg-transparent !mly-p-0 mly-shadow-none"
         sideOffset={8}
         onCloseAutoFocus={(e) => e.preventDefault()}
       >
@@ -63,21 +75,30 @@ export function LinkInputPopover(props: LinkInputPopoverProps) {
               return;
             }
 
-            onValueChange?.(input.value);
+            let value = normalizeToHttps(input.value);
+            onValueChange?.(value);
             setIsOpen(false);
           }}
         >
           <label className="relative">
-            <input
-              value={defaultValue}
-              onChange={(e) => {
-                onValueChange?.(e.target.value);
-              }}
-              ref={linkInputRef}
-              type="url"
-              className="mly-h-7 mly-w-52 mly-rounded-md mly-px-2 mly-pr-6 mly-text-sm mly-text-midnight-gray hover:mly-bg-soft-gray focus:mly-bg-soft-gray focus:mly-outline-none"
-            />
-            <div className="mly-pointer-events-none mly-absolute mly-inset-y-0 mly-right-1 mly-flex mly-items-center">
+            <div className="mly-isolate mly-flex mly-rounded-lg">
+              <span className="-mly-z-10 mly-inline-flex mly-items-center mly-rounded-s-lg mly-border mly-border-gray-300 mly-bg-gray-50 mly-px-3 mly-text-sm mly-text-gray-700">
+                https://
+              </span>
+              <input
+                value={defaultUrl}
+                onChange={(e) => {
+                  let value = normalizeToHttps(e.target.value);
+                  onValueChange?.(value);
+                }}
+                ref={linkInputRef}
+                type="text"
+                className="-mly-ms-px mly-block mly-h-8 mly-w-52 mly-rounded-lg mly-rounded-s-none mly-border mly-border-gray-300 mly-px-2 mly-py-1.5 mly-pr-6 mly-text-sm mly-shadow-sm mly-outline-none placeholder:mly-text-gray-400"
+                placeholder="maily.to/"
+              />
+            </div>
+
+            <div className="mly-pointer-events-none mly-absolute mly-inset-y-0 mly-right-1.5 mly-flex mly-items-center">
               <CornerDownLeft className="mly-h-3 mly-w-3 mly-stroke-[2.5] mly-text-midnight-gray" />
             </div>
           </label>

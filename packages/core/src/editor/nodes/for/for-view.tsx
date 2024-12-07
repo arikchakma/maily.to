3c2e25cd8c57fd020a64e66970f8cebc987149a6
Arik Chakma <arikchangma@gmail.com>
2024-12-07 16:45:20 +0600

    fix: select on sidebar click

--- packages/core/src/editor/nodes/for/for-view.tsx
+++ packages/core/src/editor/nodes/for/for-view.tsx
@@ -1,7 +1,9 @@
 import { NodeViewProps, NodeViewWrapper, NodeViewContent } from '@tiptap/react';
 import { Repeat2 } from 'lucide-react';
 
-export function ForView(_: NodeViewProps) {
+export function ForView(props: NodeViewProps) {
+  const { editor, getPos } = props;
+
   return (
     <NodeViewWrapper
       draggable="true"
@@ -11,14 +13,17 @@ export function ForView(_: NodeViewProps) {
     >
       <NodeViewContent className="is-editable" />
 
-      <div
+      <button
         data-repeat-indicator=""
-        className="mly-absolute mly-inset-y-0 mly-right-0 mly-flex mly-translate-x-full mly-flex-col mly-items-center mly-gap-1 mly-opacity-60"
+        className="mly-absolute mly-inset-y-0 mly-right-0 mly-flex mly-translate-x-full mly-cursor-pointer mly-flex-col mly-items-center mly-gap-1 mly-opacity-60"
         contentEditable={false}
+        onClick={() => {
+          editor.commands.setNodeSelection(getPos());
+        }}
       >
         <Repeat2 className="mly-size-3 mly-stroke-[2.5] mly-text-midnight-gray" />
         <div className="mly-w-[1.5px] mly-grow mly-rounded-full mly-bg-rose-300" />
-      </div>
+      </button>
     </NodeViewWrapper>
   );
 }

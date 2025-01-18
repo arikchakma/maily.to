9e61e410079760b3c32e87ad24472be352d9c6f9
Arik Chakma <arikchangma@gmail.com>
2025-01-18 18:27:25 +0600

    fix: build error

--- packages/core/src/editor/components/ui/color-picker.tsx
+++ packages/core/src/editor/components/ui/color-picker.tsx
@@ -131,6 +131,7 @@ export function ColorPicker(props: ColorPickerProps) {
                       }}
                     />
                   </BaseButton>
+                ))}
               </div>
             </div>
           )}

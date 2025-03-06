6f5066ca771e98ddbf0bbedc21e3e89523d7ac1f
Arik Chakma <arikchangma@gmail.com>
2025-03-07 05:13:49 +0600

    feat: image upload extension (#147)
    * feat: add image upload functionality with drag-and-drop and paste support
    
    * wip: initial idea of the upload image
    
    * feat: image dropzone
    
    * feat: add logo dropzone
    
    * feat: docs for image upload
    
    ---------
    
    Co-authored-by: melvynxdev <melvynmal@gmail.com>

--- packages/core/src/editor/extensions/index.ts
+++ packages/core/src/editor/extensions/index.ts
@@ -1,13 +1,13 @@
+import { VariableExtension } from '@/extensions';
 import { AnyExtension } from '@tiptap/core';
+import { HTMLCodeBlockExtension } from '../nodes/html/html';
+import { InlineImageExtension } from '../nodes/inline-image/inline-image';
 import { getVariableSuggestions } from '../nodes/variable/variable-suggestions';
 import { MailyContextType } from '../provider';
 import { MailyKit } from './maily-kit';
+import { PlaceholderExtension } from './placeholder';
 import { SlashCommandExtension } from './slash-command/slash-command';
 import { getSlashCommandSuggestions } from './slash-command/slash-command-view';
-import { VariableExtension } from '@/extensions';
-import { HTMLCodeBlockExtension } from '../nodes/html/html';
-import { InlineImageExtension } from '../nodes/inline-image/inline-image';
-import { PlaceholderExtension } from './placeholder';
 
 type ExtensionsProps = Partial<MailyContextType> & {
   extensions?: AnyExtension[];
--- packages/core/src/editor/nodes/image/image-view.tsx
+++ packages/core/src/editor/nodes/image/image-view.tsx
@@ -1,8 +1,15 @@
+import {
+  type CSSProperties,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { type NodeViewProps, NodeViewWrapper } from '@tiptap/react';
+import { Ban, BracesIcon, GrabIcon, ImageOffIcon, Loader2 } from 'lucide-react';
+import { useImageUploadOptions } from '@/editor/extensions/image-upload/image-upload';
 import { cn } from '@/editor/utils/classname';
 import { useEvent } from '@/editor/utils/use-event';
-import { type NodeViewProps, NodeViewWrapper } from '@tiptap/react';
-import { Ban, BracesIcon, ImageOffIcon, Loader2 } from 'lucide-react';
-import { type CSSProperties, useEffect, useRef, useState } from 'react';
 
 const MIN_WIDTH = 20;
 const MAX_WIDTH = 600;
@@ -13,6 +20,10 @@ export function ImageView(props: NodeViewProps) {
   const { node, updateAttributes, selected, editor } = props;
 
   const [status, setStatus] = useState<ImageStatus>('idle');
+  const [isPlaceholderImage, setIsPlaceholderImage] = useState(false);
+
+  const { onImageUpload, allowedMimeTypes = [] } =
+    useImageUploadOptions(editor);
 
   const wrapperRef = useRef<HTMLDivElement>(null);
   const imgRef = useRef<HTMLImageElement>(null);
@@ -20,6 +31,8 @@ export function ImageView(props: NodeViewProps) {
   const [resizingStyle, setResizingStyle] = useState<
     Pick<CSSProperties, 'width' | 'height'> | undefined
   >();
+
+  const [isDraggingOver, setIsDraggingOver] = useState(false);
 
   const handleMouseDown = useEvent(
     (event: React.MouseEvent<HTMLDivElement>) => {
@@ -73,25 +86,32 @@ export function ImageView(props: NodeViewProps) {
     }
   );
 
-  function dragCornerButton(direction: string) {
-    return (
-      <div
-        role="button"
-        tabIndex={0}
-        onMouseDown={handleMouseDown}
-        data-direction={direction}
-        className="mly-bg-rose-500"
-        style={{
-          position: 'absolute',
-          height: '10px',
-          width: '10px',
-          ...{ n: { top: 0 }, s: { bottom: 0 } }[direction[0]],
-          ...{ w: { left: 0 }, e: { right: 0 } }[direction[1]],
-          cursor: `${direction}-resize`,
-        }}
-      />
-    );
-  }
+  const dragCornerButton = useCallback(
+    (direction: string) => {
+      if (isPlaceholderImage) {
+        return null;
+      }
+
+      return (
+        <div
+          role="button"
+          tabIndex={0}
+          onMouseDown={handleMouseDown}
+          data-direction={direction}
+          className="mly-bg-rose-500"
+          style={{
+            position: 'absolute',
+            height: '10px',
+            width: '10px',
+            ...{ n: { top: 0 }, s: { bottom: 0 } }[direction[0]],
+            ...{ w: { left: 0 }, e: { right: 0 } }[direction[1]],
+            cursor: `${direction}-resize`,
+          }}
+        />
+      );
+    },
+    [handleMouseDown, isPlaceholderImage]
+  );
 
   let { alignment = 'center', width, height, src } = node.attrs || {};
 
@@ -104,15 +124,54 @@ export function ImageView(props: NodeViewProps) {
   } = node.attrs || {};
 
   const hasImageSrc = !!attrs.src;
+  const isDroppable =
+    !!onImageUpload &&
+    editor.isEditable &&
+    !hasImageSrc &&
+    !isSrcVariable &&
+    status === 'idle';
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isDroppable || !e.target.files || e.target.files.length === 0) {
+      return;
+    }
+
+    const file = e.target.files[0];
+    await handleImageUpload(file);
+  };
+
+  const handleImageUpload = useCallback(
+    async (file: File) => {
+      if (!isDroppable) {
+        return;
+      }
+
+      try {
+        setStatus('loading');
+        const imageUrl = await onImageUpload(file);
+        updateAttributes({ src: imageUrl });
+        setIsPlaceholderImage(false);
+        setStatus('loaded');
+      } catch (error) {
+        console.error('Error uploading image:', error);
+        setStatus('error');
+      }
+    },
+    [onImageUpload, updateAttributes]
+  );
 
   // load the image using new Image() to avoid layout shift
   // then if the image is loaded, set the status to loaded
   useEffect(() => {
-    if (!src) {
+    if (!src || isSrcVariable) {
       return;
     }
 
     setStatus('loading');
+    const isPlaceHolder =
+      editor?.extensionStorage?.imageUpload?.placeholderImages?.has(src) ??
+      false;
+    setIsPlaceholderImage(isPlaceHolder);
     const img = new Image();
     img.src = src;
     img.onload = () => {
@@ -149,11 +208,62 @@ export function ImageView(props: NodeViewProps) {
     };
   }, [src]);
 
+  const handleDragOver = useCallback(
+    (e: React.DragEvent) => {
+      if (!isDroppable) {
+        return;
+      }
+
+      e.preventDefault();
+      e.stopPropagation();
+      setIsDraggingOver(true);
+    },
+    [onImageUpload]
+  );
+
+  const handleDragLeave = useCallback(
+    (e: React.DragEvent) => {
+      if (!isDroppable) {
+        return;
+      }
+
+      e.preventDefault();
+      e.stopPropagation();
+      setIsDraggingOver(false);
+    },
+    [onImageUpload]
+  );
+
+  const handleDrop = useCallback(
+    async (e: React.DragEvent) => {
+      if (!isDroppable) {
+        return;
+      }
+
+      e.preventDefault();
+      e.stopPropagation();
+      setIsDraggingOver(false);
+      const files = e.dataTransfer?.files;
+      if (!files || files?.length === 0) {
+        return;
+      }
+
+      const firstFile = files[0];
+      if (!allowedMimeTypes.includes(firstFile.type)) {
+        return;
+      }
+
+      await handleImageUpload(firstFile);
+    },
+    [handleImageUpload]
+  );
+
   return (
     <NodeViewWrapper
       as="div"
       draggable={editor.isEditable}
       data-drag-handle={editor.isEditable}
+      className={cn('mly-image-drop-zone', isDraggingOver && 'mly-drag-over')}
       style={{
         ...(hasImageSrc && status === 'loaded'
           ? {
@@ -174,8 +284,26 @@ export function ImageView(props: NodeViewProps) {
         }[alignment as string] || {}),
       }}
       ref={wrapperRef}
+      {...(isDroppable
+        ? {
+            onDragOver: handleDragOver,
+            onDragLeave: handleDragLeave,
+            onDrop: handleDrop,
+          }
+        : {})}
     >
-      {!hasImageSrc && <ImageStatusLabel status="idle" minHeight={height} />}
+      {!hasImageSrc && status === 'idle' && (
+        <ImageStatusLabel
+          status="idle"
+          minHeight={height}
+          isDropZone={isDroppable}
+        />
+      )}
+
+      {!hasImageSrc && status === 'loading' && !isSrcVariable && (
+        <ImageStatusLabel status="loading" minHeight={height} />
+      )}
+
       {hasImageSrc && isSrcVariable && (
         <ImageStatusLabel status="variable" minHeight={height} />
       )}
@@ -185,6 +313,16 @@ export function ImageView(props: NodeViewProps) {
       )}
       {hasImageSrc && status === 'error' && !isSrcVariable && (
         <ImageStatusLabel status="error" minHeight={height} />
+      )}
+
+      {isDroppable && (
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleFileChange}
+          className="mly-absolute mly-inset-0 mly-opacity-0"
+          multiple={false}
+        />
       )}
 
       {hasImageSrc && status === 'loaded' && !isSrcVariable && (
@@ -198,9 +336,12 @@ export function ImageView(props: NodeViewProps) {
               marginBottom: 0,
             }}
             draggable={editor.isEditable}
+            className={cn(
+              isPlaceholderImage && 'mly-animate-pulse mly-opacity-40'
+            )}
           />
 
-          {selected && editor.isEditable && (
+          {selected && editor.isEditable && !isPlaceholderImage && (
             <>
               {/* Don't use a simple border as it pushes other content around. */}
               {[
@@ -233,19 +374,22 @@ export function ImageView(props: NodeViewProps) {
 type ImageStatusLabelProps = {
   status: ImageStatus | 'variable';
   minHeight?: number | string;
-};
+  isDropZone?: boolean;
+} & React.HTMLAttributes<HTMLDivElement>;
 
 export function ImageStatusLabel(props: ImageStatusLabelProps) {
-  const { status, minHeight } = props;
+  const { status, minHeight, className, style, isDropZone, ...rest } = props;
 
   return (
     <div
+      {...rest}
       className={cn(
         'mly-flex mly-items-center mly-justify-center mly-gap-2 mly-rounded-lg mly-bg-soft-gray mly-px-4 mly-py-2 mly-text-sm mly-font-medium',
         {
           'mly-text-gray-500 hover:mly-bg-soft-gray/60': status === 'loading',
           'mly-text-red-500 hover:mly-bg-soft-gray/60': status === 'error',
-        }
+        },
+        className
       )}
       style={{
         ...(minHeight
@@ -253,14 +397,23 @@ export function ImageStatusLabel(props: ImageStatusLabelProps) {
               minHeight,
             }
           : {}),
+        ...style,
       }}
     >
-      {status === 'idle' && (
+      {status === 'idle' && !isDropZone && (
         <>
           <ImageOffIcon className="mly-size-4 mly-stroke-[2.5]" />
           <span>No image selected</span>
         </>
       )}
+
+      {status === 'idle' && isDropZone && (
+        <>
+          <GrabIcon className="mly-size-4 mly-stroke-[2.5]" />
+          <span>Click or Drop image here</span>
+        </>
+      )}
+
       {status === 'loading' && (
         <>
           <Loader2 className="mly-size-4 mly-animate-spin mly-stroke-[2.5]" />

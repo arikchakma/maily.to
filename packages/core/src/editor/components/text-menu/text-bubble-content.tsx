969c6d8b0fbd2843c51ca754c37a56ae66383e07
Arik Chakma <arikchangma@gmail.com>
2025-03-08 06:42:43 +0600

    fix: remove link protocol (#151)
    * fix: remove link protocol
    
    * wip

--- packages/core/src/editor/components/text-menu/text-bubble-content.tsx
+++ packages/core/src/editor/components/text-menu/text-bubble-content.tsx
@@ -104,9 +104,7 @@ export function TextBubbleContent(props: TextBubbleContentProps) {
       <LinkInputPopover
         defaultValue={state?.linkUrl ?? ''}
         onValueChange={(value, isVariable) => {
-          const defaultValueWithoutProtocol = value.replace(/https?:\/\//, '');
-
-          if (!defaultValueWithoutProtocol) {
+          if (!value) {
             editor
               ?.chain()
               .focus()

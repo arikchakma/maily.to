ff7202a60e986c365d82c41316c7353b3352acc9
Arik Chakma <arikchangma@gmail.com>
2025-03-22 22:00:43 +0600

    feat: selection hack

---
+++ packages/core/src/editor/extensions/selection/selection.ts
@@ -0,0 +1,36 @@
+import { Extension } from '@tiptap/core';
+import { Plugin, PluginKey } from '@tiptap/pm/state';
+import { Decoration, DecorationSet } from '@tiptap/pm/view';
+
+export const SelectionExtension = Extension.create({
+  name: 'selection',
+
+  addProseMirrorPlugins() {
+    const { editor } = this;
+
+    return [
+      new Plugin({
+        key: new PluginKey('selection'),
+        props: {
+          decorations(state) {
+            if (state.selection.empty) {
+              return null;
+            }
+
+            if (editor.isFocused === true) {
+              return null;
+            }
+
+            // this is a hack to make sure the selection is visible
+            // when the editor is not focused (e.g. when trigger a popover or something similar)
+            return DecorationSet.create(state.doc, [
+              Decoration.inline(state.selection.from, state.selection.to, {
+                class: 'selection mly-bg-blue-200 mly-inline mly-py-1',
+              }),
+            ]);
+          },
+        },
+      }),
+    ];
+  },
+});

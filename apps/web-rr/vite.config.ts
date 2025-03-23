cd71e856c06e3daac8eb6c69805667b0a48338bb
Arik Chakma <arikchangma@gmail.com>
2025-03-24 04:15:26 +0600

    wip

--- apps/web-rr/vite.config.ts
+++ apps/web-rr/vite.config.ts
@@ -4,13 +4,6 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ isSsrBuild, command }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: './server/app.ts',
-        }
-      : undefined,
-  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   ssr: {
     noExternal: [/^@maily-to\//, /^@radix-ui\//, /^@tiptap\//],

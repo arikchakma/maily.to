8b9d3cbec74978a3b6a2a7376d1b0566f121b048
Arik Chakma <arikchangma@gmail.com>
2025-03-23 06:11:57 +0600

    wip

--- apps/web-rr/app/root.tsx
+++ apps/web-rr/app/root.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router';
 
 import type { Route } from './+types/root';
+import '@maily-to/core/style.css';
 import stylesheet from './app.css?url';
 import { Toaster } from 'sonner';
 import { NavigationLoadingBar } from './components/navigation-loader';

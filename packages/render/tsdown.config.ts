91946781fdcd2d50fd9502a2e6f06450b56b68c1
Arik Chakma <arikchangma@gmail.com>
2025-06-01 18:51:57 +0600

    feat: migrate render to tsdown

--- packages/render/tsdown.config.ts
+++ packages/render/tsdown.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
-// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],

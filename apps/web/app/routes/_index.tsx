bb964f10f8f0578462238a03387f8226438eb8df
Arik Chakma <arikchangma@gmail.com>
2025-06-26 00:08:44 +0600

    feat: redirect to platform

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -45,13 +45,13 @@ export default function Home(props: Route.ComponentProps) {
                 Open-source editor for crafting emails.
               </p>
               <div className="mt-4 flex flex-col items-stretch gap-2 sm:flex-row md:mt-8 lg:mt-10">
-                <Link
+                <a
                   className="flex min-h-[72px] items-center justify-center gap-3 rounded-xl bg-black px-7 py-3 text-2xl font-medium text-white transition-all hover:bg-red-500 focus:outline-0"
-                  to="/playground"
+                  href="https://app.maily.to"
                 >
                   <Icons.pencil />
                   Open Editor
-                </Link>
+                </a>
 
                 <a
                   className="flex items-center justify-center gap-2 rounded-xl border-4 border-black bg-white px-5 py-3 text-2xl font-medium text-black transition-colors hover:border-red-500 hover:bg-red-500 hover:text-white focus:outline-0"
@@ -173,13 +173,13 @@ export default function Home(props: Route.ComponentProps) {
               GitHub Stars
             </a>
 
-            <Link
+            <a
               className="flex items-center justify-center gap-2 rounded-lg border-4 border-black bg-white px-5 py-2 text-lg font-medium text-black transition-colors hover:bg-black hover:text-white md:text-xl lg:rounded-xl lg:py-3 lg:text-2xl"
-              to="/playground"
+              href="https://app.maily.to"
             >
               <Icons.pencil />
               Open Editor
-            </Link>
+            </a>
           </div>
         </div>
       </div>

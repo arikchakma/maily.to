0c2dba6d8463cad0011ec2eb7c97527f7c4591fb
Arik Chakma <arikchangma@gmail.com>
2026-01-24 04:40:15 +0600

    wip

--- packages/core/src/editor/components/text-direction-switch.tsx
+++ packages/core/src/editor/components/text-direction-switch.tsx
@@ -1,4 +1,3 @@
-import { Pilcrow } from 'lucide-react';
 import { BubbleMenuButton } from './bubble-menu-button';
 import {
   AllowedTextDirection,
@@ -7,57 +6,12 @@ import {
 import { Popover, PopoverContent, PopoverTrigger } from './popover';
 import { cn } from '../utils/classname';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
+import { LtrIcon, RtlIcon } from './icons/text-direction-icon';
 
 type TextDirectionSwitchProps = {
   direction: AllowedTextDirection;
   onDirectionChange: (direction: AllowedTextDirection) => void;
 };
-
-function LtrIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M11 5h6" />
-      <path d="M11 5v14" />
-      <path d="M15 5v14" />
-      <path d="M6 18l-3-3 3-3" />
-      <path d="M3 15h5" />
-    </svg>
-  );
-}
-
-function RtlIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M8 5h6" />
-      <path d="M10 5v14" />
-      <path d="M14 5v14" />
-      <path d="M18 18l3-3-3-3" />
-      <path d="M16 15h5" />
-    </svg>
-  );
-}
 
 export function TextDirectionSwitch(props: TextDirectionSwitchProps) {
   const { direction: rawDirection, onDirectionChange } = props;
